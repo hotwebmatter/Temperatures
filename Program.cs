96a29b750ebd6c0887e86a1a59f6e2265b1010d9
Matt Obert on Windows <matt@hotwebmatter.com>
2019-11-17 18:18:27 -0500

    Generate header

--- Program.cs
+++ Program.cs
@@ -13,9 +13,23 @@ namespace Temperatures
 {
     class Program
     {
+        // Declare array as class-level variable, per specifiction
+        private static double[] temperaturesArray = new double[7];
+        private static double averageTemperature;
+        private static double highestTemperature;
         static void Main(string[] args)
         {
-            WriteLine("Hello World!");
+            Write(GenerateHeader());
+        }
+
+        private static string GenerateHeader()
+        {
+            string result = string.Format("\tWeekly Temperature Report\n");
+            result += string.Format("\tMatthew Obert\n");
+            result += string.Format("\tNovember 17 2019\n");
+            result += string.Format("Enter 7 Temperatures\n");
+            result += string.Format("************************************\n");
+            return result;
         }
     }
 }
